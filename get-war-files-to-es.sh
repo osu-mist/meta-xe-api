1159f49c7e9f3dc42a7c10d1e8aae236b40ded4e
Jared Kosanovic <jared@Jareds-MacBook-Pro.local>
2017-02-07 08:33:40 -0800

    add shell script to update es

---
+++ get-war-files-to-es.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+XE_USER=$1
+XE_HOST=$2
+
+for instance in dev2 devl prod
+do
+    # Get all war files into a variable as a list
+    WAR_FILES=$(ssh -l $XE_USER $XE_HOST 'cd /san/smf/ban/ban9/lcl; 
+    find . -type f  -wholename "./'"$instance"'/*/current/dist/*.[[:digit:]].war" -printf "%f\n"')
+
+    for item in $WAR_FILES 
+    do 
+        APP=$(echo $item | awk -F '-' '{print $1}')
+        VERSION=$(echo $item | awk -F '-' '{print $2}' | sed 's/.war//')
+
+        # Get response code when trying to GET a specific app
+        CHECK_APP=$(curl -XGET -w %{http_code} localhost:9200/xe/apps/"$APP" | tail -c 3)
+
+        if [ "$CHECK_APP" == "404" ]
+        # If entry doesn't exist, create it
+        then
+            curl -XPUT "localhost:9200/xe/apps/$APP" -d '{
+                "applicationName": "'"$APP"'",
+                "versions": {
+                    "'"$instance"'": "'"$VERSION"'"
+                }
+            }'
+        else
+        # Else update it
+            curl -XPOST "localhost:9200/xe/apps/$APP/_update" -d '{
+                "doc": {
+                    "versions": {
+                        "'"$instance"'": "'"$VERSION"'"
+                      }
+                }
+            }'
+        fi
+    done
+done
